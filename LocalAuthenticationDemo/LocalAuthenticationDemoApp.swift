7762d6cb2f5ff9b7b4de1ca748576d2720a90631
shmn7iii <46078198+shmn7iii@users.noreply.github.com>
2022-12-07 19:47:13 +0900

    Initial Commit

---
+++ LocalAuthenticationDemo/LocalAuthenticationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalAuthenticationDemoApp.swift
+//  LocalAuthenticationDemo
+//
+//  Created by shmn7iii on 2022/12/07.
+//
+
+import SwiftUI
+
+@main
+struct LocalAuthenticationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
